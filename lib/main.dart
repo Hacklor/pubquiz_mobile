863e0217c1ed27f655d8c23110651f3fe92a18ab
Nicole Bekkers <nicole@hacklor.com>
2020-07-20 12:11:21 +0200

    Show question with choices on screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,51 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(PubQuizApp());
 }
 
-class MyApp extends StatelessWidget {
+class PubQuizApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pubquiz Mobile',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to the Pubquiz'),
-        ),
-        body: Center(
-          child: Text('Hello World'),
-        ),
+      home: QuestionPage()
+    );
+  }
+}
+
+class QuestionPage extends StatelessWidget {
+  final question = {
+    'text': 'What is the answer to life, the universe and everything?',
+    'choices': ['40', '41', '42', '43'],
+    'answer': '42'
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Question 1'),
       ),
+      body: Column(
+        children: [
+          Text(question['text']),
+          _choiceRadioListTile('41'),
+          _choiceRadioListTile('42'),
+          _choiceRadioListTile('43'),
+          _choiceRadioListTile('44'),
+        ]
+      ),
+    );
+  }
+
+  Widget _choiceRadioListTile(String choice) {
+    return RadioListTile<String>(
+      title: Text(choice),
+      value: choice,
+      groupValue: 'choices',
+      onChanged: (String value) {
+      },
     );
   }
 }
