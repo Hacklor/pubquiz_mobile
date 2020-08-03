5ae4cbe0b43c42b97cbd7546d83d7cfc1a7d2f80
Nicole Bekkers <nicole@hacklor.com>
2020-08-03 12:39:27 +0200

    Fix tests for Choices widget

--- test/choices_test.dart
+++ test/choices_test.dart
@@ -4,28 +4,32 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:pubquiz_mobile/choices.dart';
 
 void main() {
-//   group('ChoicesWidget', () {
-//     const choices = ['41', '42'];
-//
-//     testWidgets('shows the choices', (WidgetTester tester) async {
-//       await tester.pumpWidget(MaterialApp(
-//           home: Scaffold(
-//             body: Choices(choices),
-//           ),
-//       ));
-//
-//       expect(find.text('41'), findsOneWidget);
-//       expect(find.text('42'), findsOneWidget);
-//     });
-//
-//     testWidgets('selects choice', (WidgetTester tester) async {
-//       await tester.pumpWidget(MaterialApp(
-//         home: Scaffold(
-//           body: Choices(choices),
-//         ),
-//       ));
-//
-//       await tester.tap(find.text('42'));
-//     });
-//   });
+  group('ChoicesWidget', () {
+    var selectedChoice;
+    final setSelectedChoice = (value) { selectedChoice = value; };
+    const choices = ['41', '42'];
+
+    testWidgets('shows the choices', (WidgetTester tester) async {
+      await tester.pumpWidget(MaterialApp(
+          home: Scaffold(
+            body: Choices(choices, null, setSelectedChoice),
+          ),
+      ));
+
+      expect(find.text('41'), findsOneWidget);
+      expect(find.text('42'), findsOneWidget);
+    });
+
+    testWidgets('selects choice', (WidgetTester tester) async {
+      await tester.pumpWidget(MaterialApp(
+        home: Scaffold(
+          body: Choices(choices, null, setSelectedChoice),
+        ),
+      ));
+
+      await tester.tap(find.text('42'));
+
+      expect(selectedChoice, matches('42'));
+    });
+  });
 }
